cebee53f2cfb3b3f8e7999c6c3a1eaa21fbeee31
Florin Uscatu <florinusc@gmail.com>
2020-03-05 20:48:06 -0500

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Florin Uscatu on 05/03/2020.
+//  Copyright © 2020 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
